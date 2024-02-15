86071dede3712b6c6187ed3eae1c5630d97032d2
ashwinjoshyc <ashwin.joshy@mca.christuniversity.in>
2024-02-15 12:54:35 +0530

    second comm

--- simmutable.java
+++
@@ -1,9 +0,0 @@
-class simmutable{
-public static void main(String args[]){
-String a ="ashwin";
-a.concat("joshy");
-System.out.println("the value in thevariable is :"+a);
-a=a.concat("joshy");
-System.out.println("the value in thevariable is :"+a);
-}
-}
